903791eed58e549766ee8bf935308c9423122248
Suman Chatterjee <chatterjeesuman@gmail.com>
2020-03-20 15:20:52 +0000

    Initial setup

---
+++ SCAudioStream/SCAudioStream.swift
@@ -0,0 +1,18 @@
+//
+//  SCAudioStream.swift
+//  SCAudioStream
+//
+//  Created by Suman Chatterjee on 20/03/2020.
+//  Copyright © 2020 Suman Chatterjee. All rights reserved.
+//
+
+public class SCAudioStream {
+  public let title:String
+  
+  init(_ title:String) {
+    self.title = title
+  }
+  public func showTitle() -> String {
+    return title
+  }
+}
